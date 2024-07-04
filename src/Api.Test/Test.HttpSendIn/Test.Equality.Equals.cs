67fb2379c5dc5d5f5f4822a935d3992084bdfe15
pmosk <pm@ggroupp.ru>
2024-07-04 15:14:43 +0300

    Fix equality logic

---
+++ src/Api.Test/Test.HttpSendIn/Test.Equality.Equals.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace GarageGroup.Infra.Http.Api.Test;
+
+partial class HttpSendInTest
+{
+    [Fact]
+    public static void Equals_OtherIsNull_ExpectFalse()
+    {
+        var source = new HttpSendIn(HttpVerb.Get, "https://www.example.com/about");
+        var result = source.Equals(null);
+
+        Assert.False(result);
+    }
+
+    [Theory]
+    [MemberData(nameof(EqualTestData))]
+    public static void Equals_SourceIsEqualToOther_ExpectTrue(
+        HttpSendIn source, HttpSendIn other)
+    {
+        var result = source.Equals(other);
+        Assert.True(result);
+    }
+
+    [Theory]
+    [MemberData(nameof(UnequalTestData))]
+    public static void Equals_SourceIsNotEqualToOther_ExpectFalse(
+        HttpSendIn source, HttpSendIn other)
+    {
+        var result = source.Equals(other);
+        Assert.False(result);
+    }
+}
---
+++ src/Api.Test/Test.HttpSendIn/Test.Equality.EqualsWithObject.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+
+namespace GarageGroup.Infra.Http.Api.Test;
+
+partial class HttpSendInTest
+{
+    [Fact]
+    public static void EqualsWithObject_ObjectIsNull_ExpectFalse()
+    {
+        var source = new HttpSendIn(HttpVerb.Get, "https://www.example.com/about");
+        object? @object = null;
+
+        var result = source.Equals(@object);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public static void EqualsWithObject_ObjectIsNotHttpSendIn_ExpectFalse()
+    {
+        var source = new HttpSendIn(HttpVerb.Get, "https://www.example.com/products");
+        var @object = new object();
+
+        var result = source.Equals(@object);
+        Assert.False(result);
+    }
+
+    [Theory]
+    [MemberData(nameof(EqualTestData))]
+    public static void EqualsWithObject_ObjectIsEqualToSource_ExpectTrue(
+        HttpSendIn source, object @object)
+    {
+        var result = source.Equals(@object);
+        Assert.True(result);
+    }
+
+    [Theory]
+    [MemberData(nameof(UnequalTestData))]
+    public static void EqualsWithObject_ObjectIsNotEqualToSource_ExpectFalse(
+        HttpSendIn source, object @object)
+    {
+        var result = source.Equals(@object);
+        Assert.False(result);
+    }
+}
